e25f5e51a68ff758a78b0f28312fd53cc4b48e2a
vagrant <vagrant@precise64.(none)>
2016-12-09 19:59:23 +0000

    Generate user model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,8 +2,8 @@ class Survey < ActiveRecord::Base
   has_many :questions, dependent: :destroy
   has_many :responses, dependent: :destroy
   has_many :answers, dependent: :destroy
-  #has_many :users
-  #belongs-to :researcher
+  has_many :users
+  belongs_to :researcher, class_name: "User", foreign_key: "researcher_id"
 
   accepts_nested_attributes_for :questions
 end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :surveys, dependent: :destroy
+  belongs_to :survey
+
+  enum role: {user: 0, researcher: 1}
+end
